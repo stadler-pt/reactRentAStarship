b23c4aa76ab18c799757cefe8cfd92f5d7648624
Steven Adler <steven_adler@gmx.de>
2020-02-12 19:05:16 +0100

    Updates

---
+++ precache-manifest.ed5bf427dd4456a7e8ee59811705bda4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bfecae9e4fd445c3a2c9c6fcdc626d3",
+    "url": "/reactRentAStarship/index.html"
+  },
+  {
+    "revision": "e74389587f2036576b03",
+    "url": "/reactRentAStarship/static/css/main.5d6e7351.chunk.css"
+  },
+  {
+    "revision": "5027c445f89c730e55f5",
+    "url": "/reactRentAStarship/static/js/2.552411d2.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/reactRentAStarship/static/js/2.552411d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e74389587f2036576b03",
+    "url": "/reactRentAStarship/static/js/main.e30947dd.chunk.js"
+  },
+  {
+    "revision": "f5aa5d5fa4f451491a40",
+    "url": "/reactRentAStarship/static/js/runtime-main.39438600.js"
+  },
+  {
+    "revision": "6078c3c4e46e96446b84f57ee35919dc",
+    "url": "/reactRentAStarship/static/media/StarJedi.6078c3c4.woff2"
+  },
+  {
+    "revision": "c6f52b80d3465faf15909fbf6a45debd",
+    "url": "/reactRentAStarship/static/media/StarJedi.c6f52b80.woff"
+  },
+  {
+    "revision": "5993b9f33df70bcb9ac0492ef710d2c0",
+    "url": "/reactRentAStarship/static/media/ackbar.5993b9f3.jpg"
+  },
+  {
+    "revision": "9ab4c4e919bdd22c1f18f57ff73e1281",
+    "url": "/reactRentAStarship/static/media/shopping_cart.9ab4c4e9.svg"
+  },
+  {
+    "revision": "4282c3be936ecf40a80bb6ddb0d4cd2f",
+    "url": "/reactRentAStarship/static/media/solo.4282c3be.jpg"
+  },
+  {
+    "revision": "bbc27b2cc6e04971db8ecb18433b4e04",
+    "url": "/reactRentAStarship/static/media/stars.bbc27b2c.jpeg"
+  }
+]);
