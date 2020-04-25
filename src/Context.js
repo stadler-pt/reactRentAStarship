ff917e98b10d6082dba93a77118f08a6b4397667
Steven Adler <50134608+stadler-pt@users.noreply.github.com>
2020-04-25 22:04:33 +0200

    Update Context.js

--- src/Context.js
+++ src/Context.js
@@ -7,25 +7,25 @@ const ContextProvider = (props) => {
     const [cartItems, setCartItems] = useState([])
 
 useEffect(() => {
-fetch("https://swapi.co/api/starships/", {
+fetch("https://swapi.dev/api/starships/", {
   mode: 'no-cors'
 })
     .then(response => response.json())
     .then(data => {
         setProducts(data.results)
-        return fetch("https://swapi.co/api/starships/?page=2", {
+        return fetch("https://swapi.dev/api/starships/?page=2", {
   mode: 'no-cors'
 })
     }).then(response => response.json())
     .then(data => {
         setProducts(prev => prev.concat(data.results))
-        return fetch("https://swapi.co/api/starships/?page=3", {
+        return fetch("https://swapi.dev/api/starships/?page=3", {
   mode: 'no-cors'
 })
     }).then(response => response.json())
     .then(data => {
         setProducts(prev => prev.concat(data.results))
-        return fetch("https://swapi.co/api/starships/?page=4", {
+        return fetch("https://swapi.dev/api/starships/?page=4", {
   mode: 'no-cors'
 })
     }).then(response => response.json())
