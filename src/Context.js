dcc6446807715a681d6ff6cfbf342a1ce58d676c
Steven Adler <steven_adler@gmx.de>
2020-04-26 18:58:29 +0200

    changed fetch source

--- src/Context.js
+++ src/Context.js
@@ -7,28 +7,28 @@ const ContextProvider = (props) => {
     const [cartItems, setCartItems] = useState([])
 
 useEffect(() => {
-fetch("http://swapi.dev/api/starships/?page=1", {
+fetch("https://swapi.dev/api/starships/?page=1", {
     method: 'GET',
     mode: 'no-cors',
   })
     .then(response => response.json())
     .then(data => {
         setProducts(data.results)
-        return fetch("http://swapi.dev/api/starships/?page=2", {
+        return fetch("https://swapi.dev/api/starships/?page=2", {
             method: 'GET',
             mode: 'no-cors',
           })
     }).then(response => response.json())
     .then(data => {
         setProducts(prev => prev.concat(data.results))
-        return fetch("http://swapi.dev/api/starships/?page=3", {
+        return fetch("https://swapi.dev/api/starships/?page=3", {
             method: 'GET',
             mode: 'no-cors',
           })
     }).then(response => response.json())
     .then(data => {
         setProducts(prev => prev.concat(data.results))
-        return fetch("http://swapi.dev/api/starships/?page=4", {
+        return fetch("https://swapi.dev/api/starships/?page=4", {
             method: 'GET',
             mode: 'no-cors',
           })
